b29684a3e2790ad3c9926eaf0964b5afc820f15f
Filipe Cacique <felipe.cacique@hotmail.com>
2024-05-01 13:59:05 -0300

    Primeiras rotas com navigation stack e navigation tab

---
+++ src/rotas/navigation.js
@@ -0,0 +1,42 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Home from "../paginas/Home";
+import Login from "../paginas/Login";
+import Cadastro from "../paginas/Cadastro";
+import ListaPets from "../paginas/ListaPets";
+import Mensagem from "../paginas/Mensagem";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabRoutes() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="ListaPets" component={ListaPets} />
+            <Tab.Screen name="Mensagens" component={Mensagem} />
+        </Tab.Navigator>
+    )
+}
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Home"
+                screenOptions={{
+                    headerTitle: '',
+                    headerShown: false
+                }}
+            >
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                />
+                <Stack.Screen name="Login" component={Login}/>
+                <Stack.Screen name="Cadastro" component={Cadastro}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
